b4a50854a3490c3951d085388ca16ea7086f3e84
Mayuri <mayurikakde143@gmail.com>
2022-12-30 17:55:11 +0530

    gpt3

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+//import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './components';
+
+
+import './App.css';
+import { Navbar , CTA, Brand,} from './components';
+import { Header ,Footer, Blog, Possibility, Features, WhatGPT3} from './containers';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
+    </div>
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
+
+export default App;
